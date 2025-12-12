3b7fedbda0ef51c9e5a07f2c4eea9a62676969d0
Corey Engelken <corey.engelken@nxp.com>
2025-12-13 07:19:37 +0900

    Updated app version and history

--- config/version.rb
+++ config/version.rb
@@ -1,7 +1,7 @@
 module OrigenSWD
-  MAJOR = 1
-  MINOR = 1
-  BUGFIX = 3
+  MAJOR = 2
+  MINOR = 0
+  BUGFIX = 0
   DEV = nil
   VERSION = [MAJOR, MINOR, BUGFIX].join(".") + (DEV ? ".pre#{DEV}" : '')
 end
